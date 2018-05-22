a409c5b352033b1543821361d8469d7b14aac82b
JF Durk <jfdurk@hotmail.fr>
2018-05-22 01:45:19 +0000

    Revert "feat(*): transform url to html5mode"

--- app/core/core.run.js
+++ app/core/core.run.js
@@ -3,7 +3,11 @@
     
     angular
     .module('fjs.core')
-    .config($locationProvider => $locationProvider.html5Mode(true))
-    .run($rootScope => { $rootScope.search; })
+    .run(function($rootScope){
+    	$rootScope.search;
+    })
+    // .config(function($locationProvider){
+    // 	$locationProvider.html5Mode(true);
+    // })
     
 })();
